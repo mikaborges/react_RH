ffcb4d996a701fa4effe386be06a26c63541ad80
Mireli Borges <mika.oliveira1998@gmail.com>
2025-04-17 11:22:22 -0300

    Estrutura inicial

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-rose-950 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                <Link to='/home' className="text-2xl font-bold">Projeto RH</Link>
+
+                    <div className='flex gap-4'>
+                        <Link to='/temas' className='hover:underline'>Sobre-Nós</Link>
+                        <Link to='/temas' className='hover:underline'>Produto</Link> 
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,31 @@
+
+function Home() {
+    return (
+        <>
+            <div className="bg-rose-950 flex ">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                              Olá meninas
+                        </h2>
+
+                        <p className='text-xl'>
+                            Bora começar
+                        </p>
+                         
+                    </div>
+
+                    <div className="">
+                        <img
+                            src=""
+                            alt=""
+                            className=''
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
